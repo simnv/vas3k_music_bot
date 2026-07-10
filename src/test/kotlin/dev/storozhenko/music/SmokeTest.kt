f7aa5186ed836d49191c4a13aa05ba72f273955e
Valeriy Simonov <simonov@gmail.com>
2026-07-10 12:16:19 +0500

    test: add JUnit5/MockK/coroutines-test infrastructure

---
+++ src/test/kotlin/dev/storozhenko/music/SmokeTest.kt
@@ -0,0 +1,11 @@
+package dev.storozhenko.music
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class SmokeTest {
+    @Test
+    fun `test infrastructure works`() {
+        assertTrue(true)
+    }
+}
